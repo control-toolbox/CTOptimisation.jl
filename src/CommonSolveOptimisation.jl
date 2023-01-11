405fc8b87f520a10dbbe2b6ee405fc0d8d96ecd9
Olivier Cots <olivier.cots@enseeiht.fr>
2023-01-11 22:27:09 +0100

    rename

---
+++ src/CommonSolveOptimisation.jl
@@ -0,0 +1,31 @@
+module CTOptimisation
+
+using ForwardDiff: jacobian, gradient, ForwardDiff # automatic differentiation
+using LinearAlgebra # for the norm for instance
+using Printf # to print iterations results
+
+# method to compute gradient and Jacobian
+∇(f::Function, x) = ForwardDiff.gradient(f, x)
+Jac(f::Function, x) = ForwardDiff.jacobian(f, x)
+
+# dev packages
+using CTDescriptions
+#
+include("common/callbacks.jl")
+include("common/default.jl")
+include("common/exceptions.jl")
+#
+include("nlp.jl")
+#
+include("descent/structs.jl")
+include("descent/solver.jl")
+include("descent/main.jl")
+#
+export NLP, solve
+export OptimisationProblem, OptimisationInit, OptimisationSolution
+export UnconstrainedProblem
+export DescentProblem, DescentInit, DescentSol
+export OptimisationException, InconsistentArgument, IncorrectMethod
+export OptimisationCallback, PrintCallback, StopCallback
+
+end # module CTOptimisation
